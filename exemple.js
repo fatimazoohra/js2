579a7e64ecdb7f667a90ad68254da4a71901f309
Client9@EST.LAN <Client9@EST.LAN>
2019-10-15 17:11:03 +0100

    tp1

---
+++ exemple.js
@@ -0,0 +1,13 @@
+	
+	// var query = document.querySelector('#menu .item'),
+		 // queryAll= document.querySelectorAll('#menu .item span');
+
+	// alert(query.innerHTML);//affiche element 1
+
+	// alert(queryAll.length);//affiche 2
+	// alert(queryAll[0].innerHTML+' * '+queryAll[1].innerHTML); // affiche element 1* element 2
+
+	// alert(query.atributes);
+	var para = document.querySelector('contenu');
+	// document.writeln("hdfgdiidrguerg");
+	para.ClassList.add('newclass');
